d87100527fe31268b7cfc7b6946eb32509ff44b8
kitety <aizaizuori@foxmail.com>
2019-03-24 10:36:17 +0800

    基本项目搭建

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route,HashRouter } from "react-router-dom";
+import About from "./About";
+import Vip from "./Vip";
+import Main from "./Main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <div className="page-header">
+            <h1>
+              title<small>subtext</small>
+            </h1>
+          </div>
+          <Route path={"/about"} component={About} exact={true} />
+          <Route path={"/vip"} component={Vip} exact={true} />
+          <Route path={"/"} component={Main} exact={true} />
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/Vip.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Vip extends Component {
+  render() {
+    return (
+      <div>
+        Vip page
+      </div>
+    )
+  }
+}
+
