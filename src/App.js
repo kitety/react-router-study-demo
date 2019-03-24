2e9fd4d6281a8bee97b04d67a02ce3c9a7a87e58
kitety <aizaizuori@foxmail.com>
2019-03-24 13:12:43 +0800

    传参

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  NavLink,
   HashRouter,
   Link
 } from "react-router-dom";
@@ -11,6 +12,18 @@ import About from "./components/About";
 import Vip from "./components/Vip";
 import Main from "./components/Main";
 import NoMatch from "./components/Error";
+import queryString  from 'query-string'
+
+const User = (props) => {
+  const params=new URLSearchParams(props.location.search);
+  console.log(params.get('name'));
+  console.log(params.get('age'));
+  const parsed = queryString.parse(props.location.search);
+  console.log(parsed);
+  return <div>User {props.match.params.id}</div>;
+}
+
+
 
 class App extends Component {
   render() {
@@ -25,28 +38,62 @@ class App extends Component {
           <div className="app-router">
             <ul>
               <li>
-                <Link to="/">Main</Link>
+                <NavLink exact to="/">
+                  Main
+                </NavLink>
               </li>
               <li>
-                <Link to="/about">About</Link>
+                <NavLink exact to="/about">
+                  About
+                </NavLink>
               </li>
               <li>
-                <Link to="/vip">Vip</Link>
+                <NavLink exact to="/vip">
+                  Vip
+                </NavLink>
               </li>
               <li>
-                <Link to="/vi12p">404</Link>
+                <NavLink exact to="/new_home">
+                  New Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/new_home2">
+                  New Home2
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/user/nameisCool?name=zhangsan&age=12">
+                  user
+                </NavLink>
+              </li>
+              <li>
+                <NavLink exact to="/vi12p">
+                  404
+                </NavLink>
               </li>
             </ul>
             <Switch>
-            <Route path={"/"} component={Main} exact={true} />
-            <Route
-            path={"/about"}
-            component={About}
-            exact={true}
-            strict={true}
-            />
-            <Route path={"/vip"} component={Vip} exact={true} />
-            <Route component={NoMatch} strict={true} />
+              <Route path={"/"} component={Main} exact={true} />
+              <Route
+                path={"/about"}
+                component={About}
+                exact={true}
+                strict={true}
+              />
+              <Route path={"/vip"} component={Vip} exact={true} />
+              <Route path={"/user/:id"} component={User} exact={true} />
+              <Route
+                path={"/new_home"}
+                render={() => <div>New Home</div>}
+                exact={true}
+              />
+              <Route
+                path={"/new_home2"}
+                render={props => <Main {...props} name={"react-router"} />}
+                exact={true}
+              />
+              <Route component={NoMatch} strict={true} />
             </Switch>
           </div>
         </div>
