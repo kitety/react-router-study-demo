2548a9b22916ddc5228fc61d1dd8de8643137154
kitety <aizaizuori@foxmail.com>
2019-04-03 00:08:22 +0800

    prompt

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import { Prompt } from "react-router";
 
 export default class About extends Component {
+  state={
+    inpVal:''
+  }
   render() {
-    return <div>About Pages</div>;
+    return (
+      <div>
+        <Prompt message="Are you sure you want to leave?" when={!!this.state.inpVal}/>
+        <span>离开的时候提示</span>
+        <input onChange={e => this.setState({ inpVal: e.target.value })} value={this.state.inpVal}/>
+      </div>
+    );
   }
 }
